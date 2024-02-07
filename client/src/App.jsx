2e049cdd3e76a552bb9bc1bb6942fe72bcdcb3d0
DarioFGonzalez <dario.zerobyte@gmail.com>
2024-02-07 19:59:29 -0300

    basic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-import './App.css'
 import { Route, Routes, useLocation } from "react-router-dom";
-import axios from 'axios';
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import './App.css';
+import { CreateUser } from "./Components/Create/CreateUser/CreateUser";
+import { CreateVisit } from "./Components/Create/CreateVisit/CreateVisit";
+import { NavBar } from "./Components/NavBar/NavBar";
+import { Home } from './Components/Views/Home/Home';
+import { Landing } from "./Components/Views/Landing/Landing";
 
 function App() {
+  const location = useLocation();
 
   return (
     <div>
+      {location.pathname !== "/" && <NavBar />}
       <Routes>
-        
-        <Route />
-        <Route />
-        <Route />
-        <Route />
+
+        <Route path="/" element={ <Landing /> } />
+        <Route path="/home" element={ <Home /> } />
+        <Route path="/createuser" element={ <CreateUser /> } />
+        <Route path="/createvisit" element={ <CreateVisit /> } />
 
       </Routes>
     </div>
---
+++ client/src/Components/Create/CreateVisit/CreateVisit.jsx
@@ -0,0 +1,9 @@
+export const CreateVisit = () =>
+{
+
+    return(
+        <div>
+            Soy el create CreateVisit
+        </div>
+    )
+}
