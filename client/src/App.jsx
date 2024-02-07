3e5d8a067a9fd23fb6995569a1b3683389815409
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2024-02-07 19:32:09 -0300

    initial schema

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { Route, Routes, useLocation } from "react-router-dom";
+import axios from 'axios';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 
   return (
     <div>
-    
+      <Routes>
+        
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+
+      </Routes>
     </div>
   )
 }
