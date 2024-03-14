cee945ccc6fa60e8982dc04991ed70b24bd14132
DarioFGonzalez <dario.zerobyte@gmail.com>
2024-03-14 18:17:09 -0300

    Back/Ruta creada para agregar/quitar visitas de un evento ya creado

--- backend/src/Routes/eventsRouter.js
+++ backend/src/Routes/eventsRouter.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const getEvents = require('../Controllers/Events/getEvents.js');
 const postEvent = require('../Controllers/Events/postEvent.js');
 const updateEvent = require('../Controllers/Events/updateEvent.js');
+const handleVisits = require('../Controllers/Events/handleVisits.js');
 
 eventsRouter = Router();
 
 eventsRouter.get('/', getEvents);
 eventsRouter.post('/', postEvent);
 eventsRouter.put('/:id', updateEvent);
+eventsRouter.put('/', handleVisits )
 
 module.exports = eventsRouter;
