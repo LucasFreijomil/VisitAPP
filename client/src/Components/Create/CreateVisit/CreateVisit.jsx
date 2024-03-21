cfc8688e49eae719f67b8f56717f77781fe64960
DarioFGonzalez <dario.zerobyte@gmail.com>
2024-03-21 17:55:31 -0300

    pre-merge

--- client/src/Components/Create/CreateVisit/CreateVisit.jsx
+++ client/src/Components/Create/CreateVisit/CreateVisit.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { guestTypeAction, decodeUser } from '../../../Redux/actions/actions';
+import { decodeUser, guestTypeAction } from '../../../Redux/actions/actions';
 import styles from '../../Create/CreateVisit/CreateVisit.module.css';
 const defaultImage = "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png";
 
@@ -63,11 +63,6 @@ export const CreateVisit = () => {
 	};
 
 	const completedForm = { ...form, userId: activeUser.id, img: imageURL };
-	
-	useEffect(() =>
-	{
-		console.log(completedForm);
-	}, [completedForm])
 
 	// CLOUDINARY
 
